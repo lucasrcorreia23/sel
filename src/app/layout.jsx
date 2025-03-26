a6e21f0f1100de2e5b131e06e972b98ad6c1ed3f
lucas r. c. <lucas_rc15@live.com>
2025-03-26 10:39:42 -0300

    ga4 e meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,8 @@
 import '@styles/scss/style.scss';
 import "@styles/css/globals.css";
 
+import Head from 'next/head';
+
 import "@styles/css/plugins/bootstrap-grid.css";
 import "@styles/css/plugins/swiper.min.css";
 import "@styles/css/plugins/magnific-popup.css";
@@ -27,6 +29,45 @@ export const metadata = {
 const Layouts = ({ children }) => {
   return (
     <html lang="pt-br">
+      <Head>
+        {/* Meta Pixel Code */}
+        <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1699814850568555');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1699814850568555&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MZD6BW3DGE"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MZD6BW3DGE');
+          `}
+        </script>
+        {/* End Google tag (gtag.js) */}
+      </Head>
       <body>
         <div className="mil-wrapper">
           {children}
