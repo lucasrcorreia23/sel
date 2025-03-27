cc007066f620bb03feda707adb5bc626d598d9ea
lucas r. c. <lucas_rc15@live.com>
2025-03-27 13:11:43 -0300

    gtm e g4 instalados

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,6 @@
 import '@styles/scss/style.scss';
 import "@styles/css/globals.css";
 
-import Head from 'next/head';
-
 import "@styles/css/plugins/bootstrap-grid.css";
 import "@styles/css/plugins/swiper.min.css";
 import "@styles/css/plugins/magnific-popup.css";
@@ -13,7 +11,7 @@ import ScrollbarProgress from "@layouts/scrollbar-progress/Index";
 
 import Script from 'next/script';
 import WhatsappButton from './_components/WhatsappButton';
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 register();
 
@@ -21,51 +19,49 @@ import AppData from "@data/app.json";
 
 export const metadata = {
   title: {
-		default: AppData.settings.siteName,
-		template: "%s | " + AppData.settings.siteName,
-	},
+    default: AppData.settings.siteName,
+    template: "%s | " + AppData.settings.siteName,
+  },
   description: AppData.settings.siteDescription,
-}
+};
 
 const Layouts = ({ children }) => {
   return (
     <html lang="pt-br">
-      <Head>
-        {/* Meta Pixel Code */}
-        <script>
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1699814850568555');
-            fbq('track', 'PageView');
-          `}
-        </script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=1699814850568555&ev=PageView&noscript=1"
-          />
-        </noscript>
-        {/* End Meta Pixel Code */}
-
-      
-      </Head>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KKPHKZ3Q');
+            `,
+          }}
+        />
+      </head>
       <body>
+        {/* Noscript para GTM */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KKPHKZ3Q"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <div className="mil-wrapper">
           {children}
           <ScrollbarProgress />
-          <WhatsappButton /> {/* Inclua o botão do WhatsApp aqui */}
+          <WhatsappButton />
         </div>
+
+        <GoogleAnalytics gaId="G-MZD6BW3DGE" />
       </body>
-      <GoogleAnalytics gaId="G-MZD6BW3DGE" />
     </html>
   );
 };
